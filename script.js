fd40d5d767aaf8252f6f3b3b8aff0ea8f01c5855
Vivek <96946822+Vivek13130@users.noreply.github.com>
2024-04-25 23:49:03 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const plus_icons = document.querySelectorAll(".mainque img")
+console.log(plus_icons)
+
+// assigning an event listener for all + images :
+
+plus_icons.forEach((plus) => {
+    plus.addEventListener("click", () => {
+        plus.
+    })
+})
